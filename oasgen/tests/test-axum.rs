8af95addad542daeedf208a4d0fd12556b3d6f14
Jordan Grace <jgrace@knova.finance>
2026-07-23 14:33:49 -0400

    Fix nullability bugs

--- oasgen/tests/test-axum.rs
+++ oasgen/tests/test-axum.rs
@@ -5,4 +5,5 @@ fn test_axum() {
     t.pass("tests/test-axum/02-query.rs");
     t.pass("tests/test-axum/03-path.rs");
     t.pass("tests/test-axum/04-status_code.rs");
+    t.pass("tests/test-axum/05-query-option.rs");
 }
